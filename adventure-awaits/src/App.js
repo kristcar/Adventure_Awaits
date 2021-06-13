24062017bcc5e3322faf972218bb883bc84a7db3
Kristin <kristincaras1@gmail.com>
2021-06-13 19:19:20 -0400

    edits to Navbar.js

--- adventure-awaits/src/App.js
+++ adventure-awaits/src/App.js
@@ -1,11 +1,18 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Adventure Awaits</h1>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
