cf2aff848b79a5de748bbf0551793ef0f17d5517
Kristin <kristincaras1@gmail.com>
2021-06-13 20:22:15 -0400

    added in HeroSection.js and HeroSection.css

--- adventure-awaits/src/components/navbar.js
+++ adventure-awaits/src/components/navbar.js
@@ -17,13 +17,17 @@ function Navbar() {
     }
   };
 
+  useEffect(() => {
+    showButton();
+  }, []);
+
   window.addEventListener("resize", showButton);
 
   return (
     <>
       <nav className="navbar">
         <div className="navbar-container">
-          <Link to="/" className="navbar-logo">
+          <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
             Adventure Awaits <i className="fab fa-typo3"></i>
           </Link>
           <div className="menu-icon" onClick={handleClick}>
